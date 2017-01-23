0fe787bed3ae046cfa0e46ad2c83194342163316
loichu <loic.humbert@epfl.ch>
2017-01-23 09:42:46 +0100

    initialized

---
+++ inscription.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+    "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Journée Sportive du CFPT</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script type="text/javascript" src="script.js"></script>
+  </head>
+  <body>
+		
+  </body>
+</html>
+<?php
+
